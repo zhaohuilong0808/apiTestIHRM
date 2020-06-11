608620d0cf9c7c2ab1d885ac83a4bf5896af6548
15831494001 <15831494001@163.com>
2020-06-11 10:32:48 +0800

    轮巡构建

--- run_suite.py
+++ run_suite.py
@@ -18,3 +18,6 @@ report_file = "IHRM".format()
 # 4.使用BeautifulReport批量运行用例生成测试报告
 BeautifulReport(suite).report(filename=report_file, description="ihrm测试报告", log_path="./report")
 
+
+print("-" * 100)
+print("增加一行输出语句,检查轮巡构建是否生效")
